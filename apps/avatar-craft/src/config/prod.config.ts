47f41f87aea7d55c850c378277650efd980aaa9a
wuyupei <495174699@qq.com>
2023-12-29 16:07:52 +0800

    🐛

--- apps/avatar-craft/src/config/prod.config.ts
+++ apps/avatar-craft/src/config/prod.config.ts
@@ -2,7 +2,7 @@ const prodConfig = {
   mysql_server_host: '192.210.232.179',
   mysql_server_port: 3306,
   mysql_server_username: 'root',
-  mysql_server_password: '19781209Wyp',
+  mysql_server_password: '123456',
   mysql_server_database: 'avatar_craft',
 };
 
