6bc873cece8318f82357120ee1e2153214301624
wuyupei <495174699@qq.com>
2023-12-29 14:15:09 +0800

    ✨ 配置

---
+++ src/config/def.config.ts
@@ -0,0 +1,24 @@
+const defConfig = {
+  swagger_path: 'doc',
+  swagger_title: 'Avatar Craft 接口文档',
+  swagger_description: 'Avatar Craft 接口文档',
+  version: '1.0',
+
+  redis_server_host: '127.0.0.1',
+  redis_server_port: 6379,
+  redis_server_db: 1,
+
+  mysql_server_host: '127.0.0.1',
+  mysql_server_port: 3306,
+  mysql_server_username: 'root',
+  mysql_server_password: '123456',
+  mysql_server_database: 'avatar Craft',
+
+  nest_server_port: 5210,
+
+  jwt_secret: 'abvdc',
+  jwt_access_token_expires_time: '30m',
+  jwt_refresh_token_expres_time: '7d',
+};
+
+export default () => defConfig;
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(5210);
+
+  const PORT = app.get(ConfigService).get('nest_server_port');
+
+  await app.listen(PORT);
+
+  console.log(`|--> Nest is running on http://localhost:${PORT} <--|`);
 }
 bootstrap();
