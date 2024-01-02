bf36de6d70125b2fa24b35797fdea330a22bf321
wuyupei <495174699@qq.com>
2024-01-02 14:15:32 +0800

    🐛 数据库连接问题

--- apps/avatar-craft/src/config/def.config.ts
+++ apps/avatar-craft/src/config/def.config.ts
@@ -7,16 +7,6 @@ const defConfig = {
   swagger_description: 'Avatar Craft 接口文档',
   version: '1.0',
 
-  redis_server_host: '127.0.0.1',
-  redis_server_port: 6379,
-  redis_server_db: 1,
-
-  mysql_server_host: '127.0.0.1',
-  mysql_server_port: 3306,
-  mysql_server_username: 'root',
-  mysql_server_password: '123456',
-  mysql_server_database: 'avatar_craft',
-
   jwt_secret: 'abvdc',
   jwt_access_token_expires_time: '30m',
 
--- apps/avatar-craft/src/config/prod.config.ts
+++ apps/avatar-craft/src/config/prod.config.ts
@@ -2,7 +2,7 @@ const prodConfig = {
   mysql_server_host: '192.210.232.179',
   mysql_server_port: 3306,
   mysql_server_username: 'root',
-  mysql_server_password: '1234562',
+  mysql_server_password: '123456',
   mysql_server_database: 'app',
 };
 
