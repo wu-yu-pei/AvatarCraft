ff30a76135b4efb980ab0f79d3aa7acfbff0cbd2
wuyupei <495174699@qq.com>
2024-01-04 16:23:02 +0800

    ♻️ 重构逻辑

--- apps/avatar-craft/src/module/avatar_like/avatar_like.service.ts
+++ apps/avatar-craft/src/module/avatar_like/avatar_like.service.ts
@@ -2,7 +2,6 @@ import { Inject, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AvatarLike } from './avatar_like.entity';
 import { Repository } from 'typeorm';
-import { User } from '../user/user.entity';
 import { AvatarService } from '../avatar/avatar.service';
 
 @Injectable()
@@ -13,59 +12,39 @@ export class AvatarLikeService {
     @Inject(AvatarService) private readonly avatarService: AvatarService,
   ) {}
 
-  async updateLikeCount(avatar_id, user, flag) {
-    if (flag) {
-      return await this.avatarLike(avatar_id, user);
-    } else {
-      return await this.avatarUnLike(avatar_id, user);
+  async updateLikeCount(avatar_id, user) {
+    // 点赞记录
+    const likeLog = await this.findLikeLog(avatar_id, user.id);
+
+    // 1. 有记录 更新状态
+    if (likeLog) {
+      await this.changeStatus(avatar_id, user.id, -likeLog.status);
+
+      likeLog.status === 1
+        ? await this.avatarService.likeDecrement(avatar_id)
+        : await this.avatarService.likeIncrement(avatar_id);
+
+      return 'ok';
     }
+
+    // 2. 没记录 新增一条点赞记录
+    await this.createLikeLog(avatar_id, user.id);
+    await this.avatarService.likeIncrement(avatar_id);
+
+    return 'ok';
   }
 
-  // 点赞
-  async avatarLike(avatar_id: number, user: User) {
-    const isHasLike = await this.checkIsHasLikeLog(avatar_id, user.id);
+  async createLikeLog(avatar_id, user_id) {
+    const avatarLike = new AvatarLike();
 
-    if (!isHasLike) {
-      // 新增一条点赞记录
-      const avatarLike = new AvatarLike();
-      avatarLike.avatar_id = avatar_id;
-      avatarLike.status = 1;
-      avatarLike.user_id = user.id;
-      await this.avatarLikeRepository.save(avatarLike);
+    avatarLike.avatar_id = avatar_id;
+    avatarLike.status = 1;
+    avatarLike.user_id = user_id;
 
-      // 更新点赞数量
-      await this.avatarService.likeIncrement(avatar_id);
-    }
-
-    if (isHasLike && isHasLike.status === -1) {
-      // 更新点赞状态
-      await this.changeStatus(avatar_id, user.id, 1);
-
-      // 更新点赞数量
-      await this.avatarService.likeIncrement(avatar_id);
-    }
-
-    return { msg: '已点赞' };
+    await this.avatarLikeRepository.save(avatarLike);
   }
 
-  // 取消点赞
-  async avatarUnLike(avatar_id: number, user: User) {
-    const isHasLikeLog = await this.checkIsHasLikeLog(avatar_id, user.id);
-
-    if (!isHasLikeLog || isHasLikeLog.status === -1) {
-      return { code: 210, msg: '你未点赞' };
-    }
-
-    // 改变状态
-    await this.changeStatus(avatar_id, user.id, -1);
-
-    // 更新点赞数量
-    await this.avatarService.likeDecrement(avatar_id);
-
-    return { msg: '取消点赞' };
-  }
-
-  async checkIsHasLikeLog(avatar_id, user_id) {
+  async findLikeLog(avatar_id, user_id) {
     const isHasLike = await this.avatarLikeRepository.findOne({
       where: {
         avatar_id,
